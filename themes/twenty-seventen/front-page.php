e5b31260f623f347c97e6c2a6e67ffd9298ca289
Vincent Ragosta <vragosta@storycorps.org>
2017-05-22 17:40:40 -0400

    began working on navigation bar

--- themes/twenty-seventen/front-page.php
+++ themes/twenty-seventen/front-page.php
@@ -8,6 +8,22 @@
 
 get_header(); ?>
 
-<h1>Louis Casale Photography</h1>
+<section class="container">
+	<div class="col-xs-12 col-sm-3">
+		<nav>
+			<h1>Louis Casale Photography</h1>
+			<ul>
+				<li><a href="<?php echo home_url(); ?>">Home</a></li>
+				<li><a href="<?php echo home_url( '/about/' ); ?>">About</a></li>
+				<li><a href="<?php echo home_url( '/gallery/' ); ?>">Bird Gallery</a></li>
+				<li><a href="<?php echo home_url( '/blog/' ); ?>">Blog</a></li>
+				<li><a href="<?php echo home_url( '/contact/' ); ?>">Contact</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="col-xs-12 col-sm-9">
+
+	</div>
+</section>
 
 <?php get_footer(); ?>
