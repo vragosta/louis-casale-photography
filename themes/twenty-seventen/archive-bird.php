c36c142f9623e07f615abac3833009b40b6d5c45
Vincent Ragosta <vragosta@storycorps.org>
2017-05-31 17:18:04 -0400

    added gitignore file, set up archive page with lightbox, modified front page template, began constructing backend infrastructure

---
+++ themes/twenty-seventen/archive-bird.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Listen post archive
+ *
+ * @package Louis Casale Photography - Twenty Seventeen
+ * @since   0.1.0
+ */
+
+get_header();
+
+$birds = new WP_Query( [
+	'post_type'      => 'bird',
+	'posts_per_page' => -1
+] );
+
+$count = 1; ?>
+
+<?php if ( $birds->have_posts() ) : ?>
+	<section class="archive col-xs-12 col-sm-offset-3 col-sm-9">
+		<?php while ( $birds->have_posts() ) : $birds->the_post(); ?>
+			<div class="archive-item col-xs-12 col-sm-4">
+				<a href="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'large' )[0]; ?>" data-rel="lightbox" title="<?php the_excerpt(); ?>">
+					<figure class="photo">
+						<div style="background-image: url( <?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'large' )[0]; ?> );"></div>
+					</figure>
+				</a>
+				<h5><?php the_title(); ?></h5>
+			</div>
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+	</section>
+<?php endif; ?>
+
+<section class="archive col-xs-12 col-sm-offset-3 col-sm-9">
+</section>
+
+<?php get_footer(); ?>
--- themes/twenty-seventen/front-page.php
+++ themes/twenty-seventen/front-page.php
@@ -6,26 +6,52 @@
  * @since   0.1.0
 */
 
-get_header(); ?>
+get_header();
 
-<div class="container">
-	<section class="navigation col-xs-12 col-sm-2">
-		<h1><a href="<?php echo home_url(); ?>">Louis Casale Photography</a></h1>
-		<ul>
-			<li><a href="<?php echo home_url(); ?>">Home</a></li>
-			<li><a href="<?php echo home_url( '/about/' ); ?>">About</a></li>
-			<li><a href="<?php echo home_url( '/gallery/' ); ?>">Bird Gallery</a></li>
-			<li><a href="<?php echo home_url( '/blog/' ); ?>">Blog</a></li>
-			<li><a href="<?php echo home_url( '/contact/' ); ?>">Contact</a></li>
-		</ul>
-		<div class="social">
-			<a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-			<a href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-			<a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-		</div>
-	</section>
-	<section class="content col-xs-12 col-sm-offset-3 col-sm-9">
-		<div class="carousel main">
+// $featured = new WP_Query( [
+// 	'post_Type'      => 'bird',
+// 	'posts_per_page' => 6,
+// 	'meta_query'     => [
+// 		'key'     =>
+// 		'value'   =>
+// 		'compare' =>
+// 	]
+// ] );
+
+?>
+
+<section class="content col-xs-12 col-sm-offset-3 col-sm-9">
+	<div class="carousel main">
+		<figure>
+			<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1008x667.jpg' ); ?> );"></div>
+		</figure>
+		<figure>
+			<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1280x960.jpg' ); ?> );"></div>
+		</figure>
+		<figure>
+			<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1400x900.jpg' ); ?> );"></div>
+		</figure>
+		<figure>
+			<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1920x1080.jpg' ); ?> );"></div>
+		</figure>
+	</div>
+	<div class="about-excerpt">
+		<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+		<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+		<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+		<p><a href="<?php echo home_url( '/about/' ); ?>">Learn more about Louis.</a></p>
+	</div>
+	<div class="inline-navigation">
+		<h2><a href="<?php echo home_url( '/gallery/' ); ?>">Browse Bird Gallery</a></h2>
+		<div class="vertical-line"></div>
+		<h2><a href="<?php echo home_url( '/contact/' ); ?>">Contact Louis</a></h2>
+		<div class="vertical-line"></div>
+		<h2><a href="<?php echo home_url( '/blog/' ); ?>">Read The Blog</a></h2>
+	</div>
+	<div class="recent-photographs">
+		<h2>Recent Photographs</h2>
+		<div class="carousel">
 			<figure>
 				<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1008x667.jpg' ); ?> );"></div>
 			</figure>
@@ -39,58 +65,27 @@ get_header(); ?>
 				<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1920x1080.jpg' ); ?> );"></div>
 			</figure>
 		</div>
-		<div class="about-excerpt">
-			<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-			<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
-			<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
-			<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
-			<p><a href="<?php echo home_url( '/about/' ); ?>">Learn more about Louis.</a></p>
-		</div>
-		<div class="inline-navigation">
-			<h2><a href="<?php echo home_url( '/gallery/' ); ?>">Browse Bird Gallery</a></h2>
-			<div class="vertical-line"></div>
-			<h2><a href="<?php echo home_url( '/contact/' ); ?>">Contact Louis</a></h2>
-			<div class="vertical-line"></div>
-			<h2><a href="<?php echo home_url( '/blog/' ); ?>">Read The Blog</a></h2>
-		</div>
-		<div class="recent-photographs">
-			<h2>Recent Photographs</h2>
-			<div class="carousel">
-				<figure>
-					<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1008x667.jpg' ); ?> );"></div>
-				</figure>
-				<figure>
-					<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1280x960.jpg' ); ?> );"></div>
-				</figure>
-				<figure>
-					<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1400x900.jpg' ); ?> );"></div>
-				</figure>
-				<figure>
-					<div style="background-image: url( <?php echo esc_attr( LOUIS_CASALE_PHOTOGRAPHY_TEMPLATE_URL . '/assets/images/1920x1080.jpg' ); ?> );"></div>
-				</figure>
+		<a class="pull-right" href="<?php echo home_url( '/gallery/' ); ?>">View more photographs >></a>
+	</div>
+	<div class="latest-posts">
+		<h2>Latest Blog Posts</h2>
+		<div class="row">
+			<div class="blog-post col-xs-12 col-sm-6">
+				<h4>This is an example title.</h4>
+				<span>May 30, 2017</span>
+				<p>Nullam quis risus eget urna mollis ornare vel eu leo. Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 			</div>
-			<a class="pull-right" href="<?php echo home_url( '/gallery/' ); ?>">View more photographs >></a>
-		</div>
-		<div class="latest-posts">
-			<h2>Latest Blog Posts</h2>
-			<div class="row">
-				<div class="blog-post col-xs-12 col-sm-6">
-					<h4>This is an example title.</h4>
-					<span>May 30, 2017</span>
-					<p>Nullam quis risus eget urna mollis ornare vel eu leo. Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-				</div>
-				<div class="blog-post col-xs-12 col-sm-6">
-					<h4>This is an example title.</h4>
-					<span>May 27, 2017</span>
-					<p>Nullam quis risus eget urna mollis ornare vel eu leo. Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-				</div>
+			<div class="blog-post col-xs-12 col-sm-6">
+				<h4>This is an example title.</h4>
+				<span>May 27, 2017</span>
+				<p>Nullam quis risus eget urna mollis ornare vel eu leo. Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 			</div>
-			<a class="pull-right" href="<?php echo home_url( '/blog/' ); ?>">View more posts >></a>
 		</div>
-		<div class="copyright">
-			<span>Copyright &copy; 2017 Louis Casale Photography</span>
-		</div>
-	</section>
-</div>
+		<a class="pull-right" href="<?php echo home_url( '/blog/' ); ?>">View more posts >></a>
+	</div>
+	<div class="copyright">
+		<span>Copyright &copy; 2017 Louis Casale Photography</span>
+	</div>
+</section>
 
 <?php get_footer(); ?>
