3c0e5aca2dffcc5813977de6a14c3b990930232c
Vincent Ragosta <allchenzo@gmail.com>
2017-06-18 18:56:26 -0400

    added new gallery functionality, added family taxonomy

--- themes/twenty-seventen/archive-bird.php
+++ themes/twenty-seventen/archive-bird.php
@@ -6,41 +6,57 @@
  * @since   0.1.0
  */
 
+$family = $_GET['family'];
+
 get_header();
 
 $birds = new WP_Query( [
 	'post_type'      => 'bird',
 	'posts_per_page' => 16,
 	'orderby'        => 'title',
-	'order'          => 'ASC'
+	'order'          => 'ASC',
+	'tax_query'      => [
+		[
+			'taxonomy' => 'family',
+			'field'    => 'slug',
+			'terms'    => [ $family ]
+		]
+	]
 ] );
+
+// echo '<pre>';
+// var_dump( $birds->posts );
+// echo '</pre>';
 
 $count = 0;
 
 ?>
 
-<?php if ( $birds->have_posts() ) : ?>
-<section class="archive content col-xs-12 col-sm-offset-4 col-sm-8 col-md-offset-3 col-md-9">
-	<div class="row">
-		<?php while ( $birds->have_posts() ) : $birds->the_post(); ?>
-			<?php if ( $count !== 0 && $count % 4 === 0 ) { ?>
-				</div>
-				<div class="row">
-			<?php } ?>
+		<section class="archive content col-xs-12 col-sm-offset-4 col-sm-8 col-md-offset-3 col-md-9">
+	<?php if ( $birds->have_posts() ) { ?>
+		<div class="row">
+			<?php while ( $birds->have_posts() ) { ?>
+				<?php $birds->the_post(); ?>
+				<?php if ( $count !== 0 && $count % 4 === 0 ) { ?>
+					</div>
+					<div class="row">
+				<?php } ?>
 
-			<div class="archive-item col-xs-12 col-sm-6 col-md-3">
-				<a href="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'full' )[0]; ?>" data-rel="lightbox" title="<?php the_excerpt(); ?>">
-					<figure class="photo unloaded">
-						<div style="background-image: url( <?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'large' )[0]; ?> );"></div>
-					</figure>
-				</a>
-				<h5 class="unloaded"><?php the_title(); ?></h5>
-			</div>
-			<?php $count++; ?>
-		<?php endwhile; ?>
-		<?php wp_reset_postdata(); ?>
-	</div>
+				<div class="archive-item col-xs-12 col-sm-6 col-md-3">
+					<a href="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'full' )[0]; ?>" data-rel="lightbox" title="<?php the_excerpt(); ?>">
+						<figure class="photo unloaded">
+							<div style="background-image: url( <?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'large' )[0]; ?> );"></div>
+						</figure>
+					</a>
+					<h5 class="unloaded"><?php the_title(); ?></h5>
+				</div>
+				<?php $count++; ?>
+			<?php } ?>
+			<?php wp_reset_postdata(); ?>
+		</div>
+	<?php } else { ?>
+			<p style="text-align: center; padding: 40px 0;">There are no birds with this family.</p>
+	<?php } ?>
 </section>
-<?php endif; ?>
 
 <?php get_footer(); ?>
