70cef68d0eba4645d6c294b1925fa9439ff265be
Vincent Ragosta <allchenzo@gmail.com>
2017-06-18 13:09:17 -0400

    made changes gallery template

--- themes/twenty-seventen/archive-bird.php
+++ themes/twenty-seventen/archive-bird.php
@@ -10,25 +10,37 @@ get_header();
 
 $birds = new WP_Query( [
 	'post_type'      => 'bird',
-	'posts_per_page' => -1
+	'posts_per_page' => 16,
+	'orderby'        => 'title',
+	'order'          => 'ASC'
 ] );
+
+$count = 0;
 
 ?>
 
 <?php if ( $birds->have_posts() ) : ?>
 <section class="archive content col-xs-12 col-sm-offset-4 col-sm-8 col-md-offset-3 col-md-9">
+	<div class="row">
 		<?php while ( $birds->have_posts() ) : $birds->the_post(); ?>
-			<div class="archive-item col-xs-12 col-sm-6 col-md-4">
-				<a href="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'large' )[0]; ?>" data-rel="lightbox" title="<?php the_excerpt(); ?>">
+			<?php if ( $count !== 0 && $count % 4 === 0 ) { ?>
+				</div>
+				<div class="row">
+			<?php } ?>
+
+			<div class="archive-item col-xs-12 col-sm-6 col-md-3">
+				<a href="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'full' )[0]; ?>" data-rel="lightbox" title="<?php the_excerpt(); ?>">
 					<figure class="photo unloaded">
 						<div style="background-image: url( <?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'large' )[0]; ?> );"></div>
 					</figure>
 				</a>
 				<h5 class="unloaded"><?php the_title(); ?></h5>
 			</div>
+			<?php $count++; ?>
 		<?php endwhile; ?>
 		<?php wp_reset_postdata(); ?>
-	</section>
+	</div>
+</section>
 <?php endif; ?>
 
 <?php get_footer(); ?>
