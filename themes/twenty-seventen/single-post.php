c069a34216b64a4eb833cd19b4f43a06778834ce
Vincent Ragosta <allchenzo@gmail.com>
2017-06-22 13:51:45 -0400

    removed sidebar and featured image from single blog

--- themes/twenty-seventen/single-post.php
+++ themes/twenty-seventen/single-post.php
@@ -10,17 +10,9 @@ get_header();
 
 global $post; ?>
 
-<section class="col-xs-12 col-sm-offset-3 col-sm-6">
+<section class="col-xs-12 col-sm-offset-3 col-sm-9">
 	<?php while ( have_posts() ) : the_post(); ?>
-		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'large' )[0]; ?>
 		<div class="blog-item">
-			<?php if ( $image ) : ?>
-				<a href="<?php the_permalink(); ?>">
-					<figure class="photo unloaded">
-						<div style="background-image: url( <?php echo esc_attr( $image ); ?> );"></div>
-					</figure>
-				</a>
-			<?php endif; ?>
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<span><?php echo date_format( date_create( get_the_date() ), 'F jS, Y' ); ?></span>
 			<?php the_content(); ?>
@@ -28,11 +20,5 @@ global $post; ?>
 	<?php endwhile; ?>
 	<?php wp_reset_postdata(); ?>
 </section>
-
-<?php if ( is_active_sidebar( 'blog-sidebar' ) ) : ?>
-	<aside class="col-xs-12 col-sm-3">
-		<?php dynamic_sidebar( 'blog-sidebar' ); ?>
-	</aside>
-<?php endif; ?>
 
 <?php get_footer(); ?>
