175514a026bb733d9c57f72358f3024d5446806f
Vincent Ragosta <allchenzo@gmail.com>
2017-06-22 12:34:19 -0400

    added not set clause for blog, parge gallery and archive bird

--- themes/twenty-seventen/page-gallery.php
+++ themes/twenty-seventen/page-gallery.php
@@ -13,33 +13,34 @@ $families = get_terms( [
 	'hide_empty' => false,
 ] );
 
-// echo '<pre>';
-// var_dump( $families );
-// echo '</pre>';
+$count = 0;
 
-// $count = 0;
 ?>
 
 <section class="archive content col-xs-12 col-sm-offset-3 col-sm-8 col-md-offset-3 col-md-9">
-	<div class="row">
-		<?php foreach( $families as $family ) { ?>
-		<?php $image = get_term_meta( $family->term_id, 'cover_photo_url', true ); ?>
-			<?php if ( $count !== 0 && $count % 3 === 0 ) { ?>
-				</div>
-				<div class="row">
-			<?php } ?>
+	<?php if ( ! empty( $families ) ) { ?>
+		<div class="row">
+			<?php foreach( $families as $family ) { ?>
+			<?php $image = get_term_meta( $family->term_id, 'cover_photo_url', true ); ?>
+				<?php if ( $count !== 0 && $count % 3 === 0 ) { ?>
+					</div>
+					<div class="row">
+				<?php } ?>
 
-			<div class="archive-item col-xs-12 col-sm-6 col-md-4">
-				<a href="<?php echo home_url( "/birds?family=$family->slug/" ); ?>">
-					<figure class="photo unloaded">
-						<div style="background-image: url( <?php echo $image ?> );"></div>
-					</figure>
-				</a>
-				<h5 class="unloaded"><?php echo esc_html( $family->name ); ?></h5>
-			</div>
-			<?php $count++; ?>
+				<div class="archive-item col-xs-12 col-sm-6 col-md-4">
+					<a href="<?php echo home_url( "/birds?family=$family->slug/" ); ?>">
+						<figure class="photo unloaded">
+							<div style="background-image: url( <?php echo $image ?> );"></div>
+						</figure>
+					</a>
+					<h5 class="unloaded"><?php echo esc_html( $family->name ); ?></h5>
+				</div>
+				<?php $count++; ?>
+			<?php } ?>
+		</div>
+		<?php } else { ?>
+			<p class="not-set">There are no bird families currently set up.</p>
 		<?php } ?>
-	</div>
 </section>
 
 <?php get_footer(); ?>
